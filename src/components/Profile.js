1720932bf2aef8705eefa4b84b65a2200dc0533d
Mananshah20 <66172577+Mananshah20@users.noreply.github.com>
2020-12-11 15:11:28 +0530

    Update Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,9 +4,9 @@ import ProfileLeft from "./ProfileLeft"
 import ProfileRight from "./ProfileRight"
 function Profile() {
     return (
-        <div className="profileflex">
+        <div className="profile">
             <Navbar></Navbar>
-            <div className="profile">
+            <div className="profileflex">
                 <div className="left">
                     <ProfileLeft></ProfileLeft>
                 </div>
